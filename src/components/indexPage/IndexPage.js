98768032d36be44c45396a2bf3825138e79d1a02
Wwright91 <wisdomwright-martin@pursuit.org>
2024-05-10 18:01:14 -0400

    Filter Businesses By Category Or Search Input 1. Added state and functions for businesses to be filtered by category. 1. Also added function to filter by the search input.

--- src/components/indexPage/IndexPage.js
+++ src/components/indexPage/IndexPage.js
@@ -9,6 +9,7 @@ import SearchBar from "../searchBar/SearchBar";
 const API = process.env.REACT_APP_API_URL;
 
 export default function IndexPage({ businesses, findBusinessByPlaceId }) {
+  const [inputValue, setInputValue] = useState("");
   const [filteredBusinesses, setFilteredBusinesses] = useState([]);
 
   const [searchParams, setSearchParams] = useSearchParams();
@@ -35,14 +36,35 @@ export default function IndexPage({ businesses, findBusinessByPlaceId }) {
     setSearchParams({ category: e.target.id });
   };
 
+  // const filterBySearch = filteredBusinesses.filter((business) => {
+  //   return business.name.toLowerCase().includes(inputValue.toLowerCase()) || inputValue === "" && business.category === category || category === "";
+  // });
+
+  const filterBySearch = filteredBusinesses.filter((business) => {
+    return (
+      (business.name.toLowerCase().includes(inputValue.toLowerCase()) || inputValue === "") &&
+      (business.category === category || category === "")
+    );
+  });
+
+  // let businessesToDisplay = filterBySearch.length ? filterBySearch : filteredBusinesses
+  const businessesToDisplay = (inputValue || category) ? filterBySearch : filteredBusinesses;
+  
+  console.log("filterBySearch", filterBySearch, "businessesToDisplay", businessesToDisplay)
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
   return (
     <div className="IndexPage">
       <div className="IndexPage__Container">
         <div>
-          <SearchBar filteredCategory={filteredCategory} businesses={businesses} />
+          <SearchBar filteredCategory={filteredCategory} businesses={businesses} onChange={handleChange}
+            inputValue={inputValue} />
           <br/>
         <div className="IndexPage__Cards">
-          {filteredBusinesses.map((business, id) => {
+          {businessesToDisplay.map((business, id) => {
             return (
               <BusinessCard
                 business={business}
@@ -54,7 +76,7 @@ export default function IndexPage({ businesses, findBusinessByPlaceId }) {
           </div>
           </div>
         <div className="IndexPage__Map">
-          <MapContainer businesses={filteredBusinesses} />
+          <MapContainer businesses={businessesToDisplay} />
         </div>
       </div>
     </div>
